49b277417dcdf400f4149c5647c424303d43999e
garnich <garnich@tut.by>
2018-09-12 14:46:04 +0300

    'second_attempt

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+  constructor(arr) {
+		this.arr=[];
+		// your implementation
 
-  add(element) {
-    // your implementation
-  }
 
-  at(index) {
-    // your implementation
-  }
+	  }
+	  add(element) {
+	  this.element=this.arr.push(element);
+		// your implementation
 
-  get length() {
-    // your implementation
-  }
 
-  toArray() {
-    // your implementation
-  }
+	  }
+	  at(index) {
+		// your implementation
+	return this.arr[index];
 
-  sort(indices) {
+	  }
+	  get length() {
+		return this.arr.length;
+		// your implementation
+		
+
+	  }
+	  toArray() {
+		// your implementation
+		return this.arr;
+
+	  }
+	  sort(indices) {
     // your implementation
+    var val;
+		  var result = [];
+		  for(val of indices) {
+			result.push(this.arr[val]);
+		  }
+		  result.sort();
+		  for(val of indices){
+			this.arr[val]=result[val];
+		  }
+		  return  this.arr;
+
   }
 
   setComparator(compareFunction) {
     // your implementation
   }
 }
-
+let sorter = new Sorter();
 module.exports = Sorter;
